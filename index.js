0f2e98788f6dce3b3ba77ea3800cca0b73130e7a
GB-Jun <windgb369@hotmail.com>
2022-06-23 15:00:15 +0800

    font library and app.use for static folder

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+
 app.get("/", (req, res) => {
     res.send(`<h2>安安</h2>`);
 });
 
 app.use((req, res) => {
-    res.send(`<h2>404 - Not Found 找不到頁面</h2>`);
+    res.send(`<h2>404 - Not Found 找不到頁面</h2>
+    <img src="/imgs/404-error-page01.jpg" alt=""/>`);
 });
 
 app.listen(process.env.EXPRESS_PORT, () => {
