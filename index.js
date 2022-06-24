b41aa27b71a188ca6b98a0cfa51f602f864e7b7e
GB-Jun <windgb369@hotmail.com>
2022-06-24 09:50:01 +0800

    get query

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
-app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+app.get("/try-qs", (req, res) => {
+    res.json(req.query);
+});
 
 app.get("/", (req, res) => {
     res.render("main", { name: "001" });
@@ -15,11 +16,18 @@ app.post("/post", (req, res) => {
     res.end(`<h2>Post here</h2>`);
 });
 
+
+// ----------- static ------------
+app.use(express.static("public"));
+app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
+
+// ----------- 404 -------------
 app.use((req, res) => {
     res.send(`<h2>404 - Not Found 找不到頁面</h2>
     <img src="/imgs/404-error-page01.jpg" alt=""/>`);
 });
 
+// ------------ listen -----------
 app.listen(process.env.EXPRESS_PORT, () => {
     console.log(
         `Server started at http://localhost:${process.env.EXPRESS_PORT}`
